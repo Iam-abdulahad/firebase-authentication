ed305f925ad58cae7ec5f64aaaea40827092545e
Abdul Ahad <ahmmedsumon087@gmai.com>
2022-12-29 09:54:06 +0600

    button worked

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { getAuth } from 'firebase/auth';
 import app from './firebase.init';
 
-function App() {
+const auth = getAuth(app);
 
-  const auth = getAuth(app);
+function App() {
+ const handleGoogleSignIn = () =>{
+  console.log("warking")
+ };
+
+
   return (
     <div className="App">
-      <button>Sign in by Googles</button>
+      <button onClick={handleGoogleSignIn}>Sign in by Googles</button>
     </div>
   );
 }
